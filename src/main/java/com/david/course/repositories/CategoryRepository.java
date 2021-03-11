f97331830b81d1a24962a030af85b1918b7ea746
DavidsJulio <davidsjulio97@gmail.com>
2021-03-11 22:29:50 +0000

    Category

---
+++ src/main/java/com/david/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.david.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.david.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
